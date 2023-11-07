59a978f2f5f619472e12cad69bade57415c5334a
ksyuk <kaseyuki601@gmail.com>
2023-11-07 19:43:48 +0900

    initial commit

---
+++ download.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func fetchDocument(websiteURL string) (*goquery.Document, error) {
+	res, err := http.Get(websiteURL)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return nil, err
+	}
+
+	return doc, nil
+}
+
+func createImagFile(folderName, targetExt string, i int) *os.File {
+	itemName := fmt.Sprintf("%d.%s", i, targetExt)
+	filepath := path.Join(folderName, itemName)
+
+	imgFile, err := os.Create(filepath)
+	if err != nil {
+		panic(fmt.Sprintf("Error creating image file: %s", err))
+	}
+
+	return imgFile
+}
+
+func fetchImg(s *goquery.Selection, attr string) io.ReadCloser {
+	imgURL, exist := s.Attr(attr)
+	if exist != true {
+		panic(fmt.Sprintf("Error finding image url: %s", imgURL))
+	}
+	// Remove all whitespaces
+	imgURL = strings.Join(strings.Fields(imgURL), "")
+
+	imgRes, err := http.Get(imgURL)
+	if err != nil {
+		panic(fmt.Sprintf("Error creating image file: %s", err))
+	}
+
+	return imgRes.Body
+}
+
+func download(doc *goquery.Document, folderName, selector, attr, targetExt string) {
+	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
+		imgFile := createImagFile(folderName, targetExt, i)
+		defer imgFile.Close()
+
+		img := fetchImg(s, attr)
+		defer img.Close()
+
+		_, err := io.Copy(imgFile, img)
+		if err != nil {
+			panic(fmt.Sprintf("Error creating image file: %s", err))
+		}
+
+		fmt.Println("Downloaded:", i)
+	})
+}
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func receiveInput(reader *bufio.Reader, message string) string {
+	fmt.Printf("%s:", message)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error receiving input:", err)
+	}
+	input = strings.TrimSpace(input)
+
+	return input
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	webSiteURL := receiveInput(reader, "URL")
+	folderName := receiveInput(reader, "folder name")
+	selector := receiveInput(reader, "selector")
+	attr := receiveInput(reader, "attribute")
+	targetExt := receiveInput(reader, "target ext")
+
+	doc, err := fetchDocument(webSiteURL)
+	if err != nil {
+		panic(fmt.Sprintf("Error fetching document: %s", err))
+	}
+	download(doc, folderName, selector, attr, targetExt)
+
+}
